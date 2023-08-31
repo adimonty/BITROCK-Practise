13e059b7f2fa3571f9ce28a37850f0d28c6b8c4d
Aditya Mohanty <2020.aditya@gmail.com>
2023-08-31 16:33:53 +0100

    Edited fopr clarity

--- Data Structures/Exercise 5.py
+++ Data Structures/Exercise 5.py
@@ -1,9 +1,14 @@
+def get_unique_pairs(first_list, second_list):
+
+  result = set()
+  for i, j in enumerate(first_list):
+    for k in second_list:
+      result.add((j, k))
+
+  return result
+
 first_list = [2, 3, 4, 5, 6, 7, 8]
-print("First List ", first_list)
-
 second_list = [4, 9, 16, 25, 36, 49, 64]
-print("Second List ", second_list)
 
-result = zip(first_list, second_list)
-result_set = set(result)
-print(result_set)
+result = get_unique_pairs(first_list, second_list)
+print(result)
