7505394e08ce1912000860c9ee604d5b406bee0a
Aditya Mohanty <2020.aditya@gmail.com>
2023-08-31 16:30:17 +0100

    Changes

--- Data Structures/Exercise 3.py
+++ Data Structures/Exercise 3.py
@@ -1,22 +1,31 @@
-sample_list = [11, 45, 8, 23, 14, 12, 78, 45, 89]
-print("Original list ", sample_list)
+def rev_chunk(sample_list):
+  """
+  This function takes a list as input and improves the code
+  that reverses the chunks of the list.
 
-length = len(sample_list)
-chunk_size = int(length / 3)
-start = 0
-end = chunk_size
+  Args:
+    sample_list: The list to be improved.
 
-# run loop 3 times
-for i in range(3):
+  Returns:
+    None.
+  """
+
+  length = len(sample_list)
+  chunk_size = int(length / 3)
+  start = 0
+  end = chunk_size
+
+  # run loop 3 times
+  for i, _ in enumerate(sample_list):
     # get indexes
     indexes = slice(start, end)
     
-    # get chunk
-    list_chunk = sample_list[indexes]
-    print("Chunk ", i, list_chunk)
-    
-    # reverse chunk
-    print("After reversing it ", list(reversed(list_chunk)))
+    # reverse chunk in place
+    sample_list[indexes] = list(reversed(sample_list[indexes]))
+    print("Chunk ", i, sample_list[indexes])
 
     start = end
     end += chunk_size
+
+sample_list = [11, 45, 8, 23, 14, 12, 78, 45, 89]
+rev_chunk(sample_list)
