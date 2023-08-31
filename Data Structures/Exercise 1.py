a243001a2b81eba37d98ba440007cc1c3badf909
Aditya Mohanty <2020.aditya@gmail.com>
2023-08-31 16:25:32 +0100

    Edited code

--- Data Structures/Exercise 1.py
+++ Data Structures/Exercise 1.py
@@ -1,16 +1,9 @@
+def odd_even_list(list1, list2):
+  odd_even_elements = list(zip(list1[1::2], list2[0::2]))
+  return list(enumerate(odd_even_elements))
+
 list1 = [3, 6, 9, 12, 15, 18, 21]
 list2 = [4, 8, 12, 16, 20, 24, 28]
-res = list()
 
-odd_elements = list1[1::2]
-print("Element at odd-index positions from list one")
-print(odd_elements)
-
-even_elements = list2[0::2]
-print("Element at even-index positions from list two")
-print(even_elements)
-
-print("Printing Final third list")
-res.extend(odd_elements)
-res.extend(even_elements)
-print(res)
+odd_even_list = odd_even_list(list1, list2)
+print(odd_even_list)
