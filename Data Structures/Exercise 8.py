47a01746888fee9e13ba40521730f80708dd8605
Aditya Mohanty <2020.aditya@gmail.com>
2023-08-31 16:38:26 +0100

    Edited

--- Data Structures/Exercise 8.py
+++ Data Structures/Exercise 8.py
@@ -1,9 +1,18 @@
+def remove_unwanted_elements(roll_number, sample_dict):
+
+  unwanted_elements = []
+  for item in roll_number:
+    if item not in sample_dict.values():
+      unwanted_elements.append(item)
+
+  for item in unwanted_elements:
+    roll_number.remove(item)
+
+  return roll_number
+
 roll_number = [47, 64, 69, 37, 76, 83, 95, 97]
 sample_dict = {'Jhon': 47, 'Emma': 69, 'Kelly': 76, 'Jason': 97}
 
-print("List:", roll_number)
-print("Dictionary:", sample_dict)
+roll_number = remove_unwanted_elements(roll_number, sample_dict)
 
-# create new list
-roll_number[:] = [item for item in roll_number if item in sample_dict.values()]
 print("after removing unwanted elements from list:", roll_number)
