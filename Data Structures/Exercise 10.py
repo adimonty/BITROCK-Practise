04d55f6d09d8777e9a810840a21866f3e16cbaa0
Aditya Mohanty <2020.aditya@gmail.com>
2023-08-31 16:36:38 +0100

    Edited

--- Data Structures/Exercise 10.py
+++ Data Structures/Exercise 10.py
@@ -1,12 +1,14 @@
+def find_min_max_unique_numbers(list1):
+
+  unique_list = list(set(list1))
+  min_number = min(unique_list)
+  max_number = max(unique_list)
+
+  return min_number, max_number
+
 sample_list = [87, 52, 44, 53, 54, 87, 52, 53]
 
-print("Original list", sample_list)
+min_number, max_number = find_min_max_unique_numbers(sample_list)
 
-sample_list = list(set(sample_list))
-print("unique list", sample_list)
-
-t = tuple(sample_list)
-print("tuple ", t)
-
-print("Minimum number is: ", min(t))
-print("Maximum number is: ", max(t))
+print("Minimum number is: ", min_number)
+print("Maximum number is: ", max_number)
