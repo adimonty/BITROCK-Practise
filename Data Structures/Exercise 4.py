d0694471946cd411a04c52e16aeaa5fb61c8e738
Aditya Mohanty <2020.aditya@gmail.com>
2023-08-31 16:32:51 +0100

    Changes

--- Data Structures/Exercise 4.py
+++ Data Structures/Exercise 4.py
@@ -1,11 +1,9 @@
 sample_list = [11, 45, 8, 11, 23, 45, 23, 45, 89]
 print("Original list ", sample_list)
 
-count_dict = dict()
+count_dict = {}
 for item in sample_list:
-    if item in count_dict:
-        count_dict[item] += 1
-    else:
-        count_dict[item] = 1
+  count = count_dict.get(item, 0)
+  count_dict[item] = count + 1
 
 print("Printing count of each item  ", count_dict)
