7de00488a2a19315880aa7a446602ada309560f7
Aditya Mohanty <2020.aditya@gmail.com>
2023-08-31 16:34:41 +0100

    Improved

--- Data Structures/Exercise 6.py
+++ Data Structures/Exercise 6.py
@@ -1,12 +1,13 @@
+def remove_common_elements(first_set, second_set):
+
+  intersection = first_set.intersection(second_set)
+  for item in intersection:
+    first_set.remove(item)
+
+  return first_set
+
 first_set = {23, 42, 65, 57, 78, 83, 29}
 second_set = {57, 83, 29, 67, 73, 43, 48}
 
-print("First Set ", first_set)
-print("Second Set ", second_set)
-
-intersection = first_set.intersection(second_set)
-print("Intersection is ", intersection)
-for item in intersection:
-    first_set.remove(item)
-
+first_set = remove_common_elements(first_set, second_set)
 print("First Set after removing common element ", first_set)
